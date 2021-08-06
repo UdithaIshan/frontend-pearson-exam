6919dca8ef82a3e9d40d554e5cce3d5b667c8289
UdithaIshan <uditha.ishan34@gmail.com>
2021-08-06 19:13:57 +0530

    routes created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { CountriesComponent } from './pages/countries/countries.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/countries',
+    pathMatch: 'full'
+  },
+  {
+    path: 'countries',
+    component: CountriesComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
